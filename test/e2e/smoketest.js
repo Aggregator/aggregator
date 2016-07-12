98316e7d9f1c01ca6fa80d39c7a85e47c3addabc
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2016-07-11 22:42:37 -0400

    chore(setup): #12 setup e2e test task

---
+++ test/e2e/smoketest.js
@@ -0,0 +1,11 @@
+const WAIT = 200;
+
+module.exports = {
+  'Smoketest'(browser) {
+    browser
+      .url('http://localhost:8080/')
+      .waitForElementVisible('body', WAIT)
+      .assert.containsText('.brand-logo', 'Aggregator')
+      .end();
+  }
+};
