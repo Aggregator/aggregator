b7ce0737fa8addb6f80d7cdae9d4bd65f81e5210
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2016-07-18 00:43:09 -0400

    chore(api): add endpoint route mocks

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ import express from 'express'
 
 const app = express()
 
+import pipelines from './routes/pipelines'
+import projects from './routes/projects'
+
 app.set('port', process.env.PORT || 3000)
 
 app.use('/', express.static('public'))
+
+app.use('/pipelines', pipelines)
+app.use('/projects', projects)
 
 app.listen(app.get('port'), () => console.log(`server is up ${app.get('port')}`))
