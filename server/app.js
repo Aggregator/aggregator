4a38fbf77ef0d7605a50bb0d659b3320f5e79820
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2016-07-26 22:47:18 -0400

    chore(api): update swagger ui

--- server/app.js
+++ server/app.js
@@ -2,14 +2,8 @@ import express from 'express'
 
 const app = express()
 
-import pipelines from './routes/pipelines'
-import projects from './routes/projects'
+const PORT = process.env.PORT || 3000
 
-app.set('port', process.env.PORT || 3000)
+app.use(express.static('public'))
 
-app.use('/', express.static('public'))
-
-app.use('/pipelines', pipelines)
-app.use('/projects', projects)
-
-app.listen(app.get('port'), () => console.log(`server is up ${app.get('port')}`))
+app.listen(PORT)
